c93cbca3a18858f923147e636e9a9ed73293013e
feder08 <fedelouit@gmail.com>
2024-07-11 15:58:23 -0700

    fontsresponsive

--- src/components/SectionHome.jsx
+++ src/components/SectionHome.jsx
@@ -8,11 +8,11 @@ export function SectionHome({imgHome, homeSlogan, homeInfo, btnWelcome}) {
 				backgroundImage: `url(${imgHome})`,
 			}}
 		>
-			<article className="w-full md:w-1/2 text-neutral-300  text-center">
-				<h1 className="text-3xl lg:text-6xl xl:text-7xl mb-8 md:text-left">
+			<article className="w-full md:w-1/2 text-neutral-100  text-center">
+				<h1 className="text-5xl lg:text-6xl xl:text-7xl mb-8   bg-lime-200 bg-opacity-35 rounded-md md:bg-transparent md:bg-opacity-0">
 					<strong>{homeSlogan}</strong>
 				</h1>
-				<p className="text-lg  lg:text-xl xl:text-3xl mt-16 mb-16 sm:font-bold md:text-left text-lime-100">
+				<p className="text-lg  lg:text-xl xl:text-3xl mt-16 mb-16  md:text-left text-lime-200 bg-black bg-opacity-50  rounded-md">
 					{homeInfo}
 				</p>
 				<button className="mt-16 px-6 py-3 bg-lime-200 text-neutral-600 font-bold rounded-lg hover:bg-lime-400 hover:text-neutral-100 sm:text-lg shadow-sm hover:shadow-sm transition-transform duration-300 ease-in-out">
