e373417a95b8ee22830155f182b6bb2b42952f79
feder08 <fedelouit@gmail.com>
2024-07-15 22:40:20 -0700

    unificarnavbarymobile

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,4 @@
-import {NavBar} from "./NavBar";
-import logo from "../assets/images/loguito.png";
+import NavBar from "./NavBar";
 import SectionHome from "./SectionHome";
 import imgHome from "../assets/images/background.jpg";
 import CardHome from "./CardHome";
@@ -7,16 +6,7 @@ import CardHome from "./CardHome";
 const App = () => {
 	return (
 		<>
-			<NavBar
-				logo={logo}
-				btnNav={[
-					{label: "Home", link: "#home"},
-					{label: "Services", link: "#services"},
-					{label: "About us", link: "#about"},
-					{label: "Contact us", link: "#contact"},
-				]}
-				logoLink="#home"
-			/>
+			<NavBar />
 			<SectionHome
 				imgHome={imgHome}
 				homeSlogan="¿Listo para alcanzar tus metas?"
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,47 +1,53 @@
-//Validacion de los PROP q voy a usar en un funcion para evitar posibles errores
-import PropTypes from "prop-types";
-//importar useState para usarlo en mi funcion NavBar y useEffect para añadir icono hamburguesa.
 import {useState, useEffect} from "react";
 import {FaBars, FaTimes} from "react-icons/fa";
-import MobileMenu from "./MobileMenu";
+import logo from "../assets/images/loguito.png";
 
-//Creo una funcion con las propiedad deseadas para mi Navigation Bar
-export function NavBar({logo, btnNav, logoLink}) {
-	//Creo una variable para AGREGAR componentes dinamicos
-	const [activeSection, setActiveSection] = useState("Home");
-	const [menuOpen, setMenuOpen] = useState(false);
-	const toggleMenu = () => {
-		setMenuOpen(!menuOpen);
-	};
+const labels = {
+	HOME: "Home",
+	SERVICES: "Services",
+	ABOUT: "About",
+	CONTACT: "Contact",
+};
+
+const NavBar = () => {
+	const [activeSection, setActiveSection] = useState(labels.HOME);
+	const [isOpen, setIsOpen] = useState(false);
+
+	const nav = [
+		{label: labels.HOME, link: "#home"},
+		{label: labels.SERVICES, link: "#services"},
+		{label: labels.ABOUT, link: "#about"},
+		{label: labels.CONTACT, link: "#contact"},
+	];
+
+	const toggleMenu = () => setIsOpen(!isOpen);
 	useEffect(() => {
-		const handleScrollAndResize = () => {
-			if (menuOpen) {
-				setMenuOpen(false);
-			}
-		};
-		window.addEventListener("scroll", handleScrollAndResize);
-		window.addEventListener("resize", handleScrollAndResize);
+		const closeMenu = () => setIsOpen(false);
+		window.addEventListener("scroll", closeMenu);
+		window.addEventListener("resize", closeMenu);
 
 		return () => {
-			window.removeEventListener("scroll", handleScrollAndResize);
-			window.removeEventListener("resize", handleScrollAndResize);
+			window.removeEventListener("scroll", closeMenu);
+			window.removeEventListener("resize", closeMenu);
 		};
-	}, [menuOpen]);
+	}, []);
+
+	const menuClass = isOpen
+		? "transform translate-x-0 opacity-100"
+		: "transform translate-x-full opacity-0";
 
 	return (
 		<header>
 			<nav className=" bg-lime-200 bg-opacity-75 flex items-center justify-between shadow-sm ">
 				<div className="flex items-center pl-4">
-					<a href={logoLink} className="flex items-center pl-4">
+					<a href={"#home"} className="flex items-center pl-4">
 						<img className="size-16 " src={logo} alt="Logo" />
 					</a>
 					{/* depend on size-x see mobileMenu */}
 				</div>
 				<ul className=" space-x-8 justify-end pr-4 hidden md:flex">
-					{/* Al usar map en el prop btnNav este espera que le pase un array y recibir 2 parametros btn e index */}
-					{btnNav.map((btn, index) => (
+					{nav.map((btn, index) => (
 						<li key={index}>
-							{/*Cada iteracion me devuelve un LI con un A Link va ser la el A e indicara si esta seleccionado o no. Dentro le pongo una clase con un operado ternarario que me permite cambiar el estado de la variable activeSection*/}
 							<a
 								href={btn.link}
 								className={`py-2 px-4 rounded-md transition duration-300 ease-in-out ${
@@ -56,60 +62,52 @@ export function NavBar({logo, btnNav, logoLink}) {
 								}}
 							>
 								{btn.label}
-								{/*btn.label accede al valor de la propiedad label de ese objeto, que es el texto que se mostrará como contenido del enlace <a>*/}
 							</a>
 						</li>
 					))}
 				</ul>
-				<button
-					className="flex md:hidden pr-4 transition-transform duration-300 ease-in-out"
-					onClick={toggleMenu}
-				>
-					{menuOpen ? (
-						<FaTimes className=" text-gray-500" />
-					) : (
-						<FaBars className=" text-gray-500" />
-					)}
-				</button>
-				{menuOpen && (
-					<MobileMenu
-						btnNav={btnNav}
-						activeSection={activeSection}
-						setActiveSection={setActiveSection}
-						toggleMenu={toggleMenu}
-						menuOpen={menuOpen}
-					/>
-				)}
+				<div className="flex md:hidden pr-4">
+					<button
+						className="transition-transform duration-300 ease-in-out"
+						onClick={toggleMenu}
+					>
+						{isOpen ? (
+							<FaTimes className="text-gray-500" />
+						) : (
+							<FaBars className="text-gray-500" />
+						)}
+					</button>
+				</div>
 			</nav>
+			{isOpen && (
+				<div
+					className={`fixed top-16 right-0 bottom-0 items-center justify-center z-50 bg-lime-200 bg-opacity-80 w-1/2 h-auto rounded-b-md transition-transform duration-300 ease-in-out shadow-sm max-h-full overflow-y-auto ${menuClass}`}
+				>
+					<ul className="space-y-4 p-2">
+						{nav.map((btn, index) => (
+							<li key={index}>
+								<a
+									href={btn.link}
+									className={`block text-center py-2 px-4 rounded-md transition duration-300 ease-in-out ${
+										activeSection === btn.label
+											? "font-bold cursor-default text-neutral-600"
+											: "text-neutral-800 hover:bg-lime-400 hover:text-neutral-100 hover:shadow-sm"
+									}`}
+									onClick={() => {
+										if (activeSection !== btn.label) {
+											setActiveSection(btn.label);
+											toggleMenu();
+										}
+									}}
+								>
+									{btn.label}
+								</a>
+							</li>
+						))}
+					</ul>
+				</div>
+			)}
 		</header>
 	);
-}
-//Validacion de los PROP q voy a usar en un funcion para evitar posibles errores
-//Esto hace que cada prop reciba la informacion requerida y no pueda recibir otro tipo de dato
-NavBar.propTypes = {
-	logo: PropTypes.string.isRequired,
-	btnNav: PropTypes.arrayOf(
-		PropTypes.shape({
-			label: PropTypes.string.isRequired,
-			link: PropTypes.string.isRequired,
-		})
-	).isRequired,
-	logoLink: PropTypes.string.isRequired,
 };
-
 export default NavBar;
-
-//PARA IMPORTAR EL COMPONENTE EN OTRO ARCHIVO
-{
-	/* <NavBar
-logo=".asset/loguito.svg"
-btnNav={[
-    {label: "Home", link: "#home"},
-    {label: "Services", link: "#services"},
-    {label: "About us", link: "#about"},
-    {label: "Contact us", link: "#contac"},
-]}
-logoLink="#home"
-/>
-); */
-}
