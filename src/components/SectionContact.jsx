15a9e5a0d0618317df28e3676844f4a426859c85
feder08 <fedelouit@gmail.com>
2024-07-22 15:49:56 -0700

    sectionContact

--- src/components/SectionContact.jsx
+++ src/components/SectionContact.jsx
@@ -1,3 +1,5 @@
+import bgContact from "../assets/images/gymContact.jpg";
+
 const Contact = () => {
 	const contactTittle = "¿Quieres Saber Más?";
 	const contactSubtittle =
@@ -41,13 +43,15 @@ const Contact = () => {
 		address: {
 			addressLine: "Av. Siempreviva 743 ",
 			city: "Spingfield",
-			province: "",
-			postalCode: "",
-			country: "",
-			map: "",
+			province: "Esta Lejos",
+			postalCode: "31246",
+			country: "United States",
 		},
 
-		contactDetails: {phone: "", email: ""},
+		contactDetails: {
+			phone: "321-654-9870",
+			email: "contant@spartanfitness.com",
+		},
 	};
 
 	const tittlesInfoContact = {
@@ -57,63 +61,97 @@ const Contact = () => {
 	};
 
 	return (
-		<section className=" bg-lime-400">
-			<div className="" style={{backgroundImage: ``}}>
-				<h1>{contactTittle}</h1>
-				<h3>{contactSubtittle}</h3>
-			</div>
-			<article>
-				<h1>{tittleForm}</h1>
-				<form className="">
-					{formFields.map((field, index) => (
-						<div key={index} className="">
-							<label htmlFor={field.name} className="">
-								{field.label}
-							</label>
-							{field.type === "textarea" ? (
-								<textarea
-									id={field.name}
-									name={field.name}
-									className=""
-									rows="10"
-									placeholder={field.placeholder}
-								></textarea>
-							) : (
-								<input
-									id={field.name}
-									type={field.type}
-									name={field.name}
-									className=""
-									placeholder={field.placeholder}
-								/>
-							)}
+		<>
+			<section
+				className="bg-cover bg-center flex flex-col justify-center items-start px-2  font-sans"
+				style={{backgroundImage: `url(${bgContact})`}}
+			>
+				<div className="w-full text-neutral-200 bg-black bg-opacity-50 text-center ">
+					<h1 className=" mt-10 text-6xl sm:text-7xl lg:text-8xl mx-auto mb-16 md:mb-24 lg:mb-32">
+						<strong>{contactTittle}</strong>
+					</h1>
+					<h2 className="text-lg sm:text-2xl md:text-3xl lg:text-4xl mx-2 my-8 rounded-md">
+						{contactSubtittle}
+					</h2>
+				</div>
+			</section>
+			<section className="flex flex-col md:flex-row justify-between items-start md:items-start w-full bg-neutral-300">
+				<article className="flex flex-col w-full md:w-1/2 p-4">
+					<h1 className="text-2xl font-semibold mb-4">{tittleForm}</h1>
+					<form className="w-full ">
+						{formFields.map((field, index) => (
+							<div key={index} className="mb-4">
+								<label
+									htmlFor={field.name}
+									className="block text-lg font-medium mb-1"
+								>
+									{field.label}
+								</label>
+								{field.type === "textarea" ? (
+									<textarea
+										id={field.name}
+										name={field.name}
+										className="w-full p-2 border border-gray-300 rounded"
+										rows="4"
+										placeholder={field.placeholder}
+									></textarea>
+								) : (
+									<input
+										id={field.name}
+										type={field.type}
+										name={field.name}
+										className="w-full p-2 border border-gray-300 rounded"
+										placeholder={field.placeholder}
+									/>
+								)}
+							</div>
+						))}
+						<div className="flex justify-center md:justify-end">
+							<button
+								type="submit"
+								className=" bg-lime-200 text-neutral-600 font-bold rounded-lg hover:bg-lime-400 hover:text-neutral-100 py-2 px-4 text-center"
+							>
+								Enviar
+							</button>
 						</div>
-					))}
-				</form>
-			</article>
-			<article>
-				<h2>{contactInfo}</h2>
-				<div>
-					<h3>{tittlesInfoContact.ADDRESS}</h3>
-					<p>{infoContact.address.addressLine}</p>
-					<p>{infoContact.address.city}</p>
-					<p>{infoContact.address.province}</p>
-					<p>{infoContact.address.postalCode}</p>
-					<p>{infoContact.address.country}</p>
-					{infoContact.address.map && (
-						<img src={infoContact.address.map} alt="Location Map" />
-					)}
-				</div>
-				<div>
-					<h3>{tittlesInfoContact.PHONE}</h3>
-					<p></p>
-				</div>
-				<div>
-					<h3>{tittlesInfoContact.EMAIL}</h3>
-					<p></p>
-				</div>
-			</article>
-		</section>
+					</form>
+				</article>
+				<article className="flex flex-col w-full md:w-1/2 p-4">
+					<h2 className="text-2xl font-semibold mb-4">{contactInfo}</h2>
+					<div className="mb-4">
+						<h3 className="text-lg font-medium">
+							{tittlesInfoContact.ADDRESS}
+						</h3>
+						<p>{infoContact.address.addressLine}</p>
+						<p>{infoContact.address.city}</p>
+						<p>{infoContact.address.province}</p>
+						<p>{infoContact.address.postalCode}</p>
+						<p>{infoContact.address.country}</p>
+						<div className="w-full h-64 relative overflow-hidden">
+							<iframe
+								src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d209225.3883869778!2d-82.30585247828715!3d34.97799014841222!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48278173fd277d7%3A0xb2f44e73d004c6fc!2sSpartan%20City%20Fitness%2C%20LLC!5e0!3m2!1sen!2sca!4v1721686733658!5m2!1sen!2sca"
+								width="100%"
+								height="100%"
+								style={{border: 0}}
+								allowFullScreen=""
+								loading="lazy"
+								referrerPolicy="no-referrer-when-downgrade"
+								title="Google Maps Location"
+								className="absolute top-0 left-0 w-full h-full"
+							></iframe>
+						</div>
+					</div>
+					<div className="mb-4">
+						<h3 className="text-lg font-medium">{tittlesInfoContact.PHONE}</h3>
+						<p>{infoContact.contactDetails.phone}</p>
+					</div>
+					<div className="mb-4">
+						<h3 className="text-lg font-medium">{tittlesInfoContact.EMAIL}</h3>
+						<p>{infoContact.contactDetails.email}</p>
+					</div>
+				</article>
+			</section>
+		</>
 	);
 };
 
